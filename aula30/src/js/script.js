a3d2a1cee2d61f398270582ab04054c217c8f4cd
Raphael Rodrigues <professor.dev.raphael@gmail.com>
2022-11-22 20:22:46 -0300

    Concluindo e Melhorando Exercicio Aula 30

--- aula30/src/js/script.js
+++ aula30/src/js/script.js
@@ -7,18 +7,29 @@ window.addEventListener("DOMContentLoaded", () => {
 
   let botao = document.querySelector(".btn");
 
-  const adicionarDados = () => {
+  const adicionarDados = (data) => {
     if (
       parcela.value.length !== 0 &&
       vencimento.value.length !== 0 &&
       valor.value.length !== 0
     ) {
+      preencherDados(data)
     } else {
       alert("ERRO! DIGITE VALORES NAS CAIXAS");
     }
   };
 
   botao.addEventListener("click", () => {
-    adicionarDados();
+    adicionarDados(dados);
   });
+
+  const preencherDados = (data) => {
+    data.innerHTML += `
+        <tr>
+          <td>${parcela.value}</td>
+          <td>${vencimento.value}</td>
+          <td>${valor.value}</td>
+        </tr>
+      `;
+  }
 });
