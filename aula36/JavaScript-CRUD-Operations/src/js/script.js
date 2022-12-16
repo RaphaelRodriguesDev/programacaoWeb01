13e7ada0ce23c3b625d55b445fe070e1a35081eb
Raphael Rodrigues <professor.dev.raphael@gmail.com>
2022-12-16 10:22:03 -0300

    aula36 - update

--- aula36/JavaScript-CRUD-Operations/src/js/script.js
+++ aula36/JavaScript-CRUD-Operations/src/js/script.js
@@ -1,7 +1,7 @@
 let selectedRow = null
 
-// function exceuted when form is submitted
-function onFormSubmit() {
+// Função executada quando o formulário é enviado
+const onFormSubmit = () => {
     if (validate()) {
         let formData = readFormData();
         if (selectedRow == null)
@@ -12,67 +12,73 @@ function onFormSubmit() {
     }
 }
 
-// function to read recieve data submitted by user
-function readFormData() {
+// READ
+const readFormData = () => {
     let formData = {};
-    formData.fullName = document.getElementById("fullName").value;
-    formData.empCode = document.getElementById("empCode").value;
-    formData.salary = document.getElementById("salary").value;
-    formData.city = document.getElementById("city").value;
+    formData.fullName = document.querySelector("#fullName").value;
+    formData.empCode = document.querySelector("#empCode").value;
+    formData.salary = document.querySelector("#salary").value;
+    formData.city = document.querySelector("#city").value;
     return formData; 
 }
 
-//function to display submitted data in a different table
-function insertNewRecord(data) {
-    let table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
-    // create a new row and insert data continuosly over the lenght of table
-    let newRow = table.insertRow(table.length); // table.length for the subsquent data to be submitted
+// CREATE
+const insertNewRecord = (data) => {
+    let table = document.querySelector("#employeeList").getElementsByTagName('tbody')[0];
+    // Cria uma nova linha e insere dados continuamente ao longo do comprimento da tabela (table.length)
+    let newRow = table.insertRow(table.length); // table.length para os dados subsequentes a serem enviados
     cell1 = newRow.insertCell(0); 
-    cell1.innerHTML = data.fullName; //cell 1 of row 1
+    cell1.innerHTML = data.fullName; //celula 1 da linha 1
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.empCode; //cell 2 of row 1
+    cell2.innerHTML = data.empCode; //celula 2 da linha 1
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary; //cell 3 of row 1
+    cell3.innerHTML = data.salary; //celula 3 da linha 1
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;   //cell 4 of row 1
+    cell4.innerHTML = data.city;   //celula 4 da linha 1
     cell5 = newRow.insertCell(4); 
-    cell5.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                       <a onClick="onDelete(this)">Delete</a>`;
+    cell5.innerHTML = `<a onClick="onEdit(this)">Editar</a>
+                       <a onClick="onDelete(this)">Deletar</a>`;
 }
 
-function resetForm() {
-    document.getElementById("fullName").value = "";
-    document.getElementById("empCode").value = "";
-    document.getElementById("salary").value = "";
-    document.getElementById("city").value = "";
+// Função para RESETAR o formulário
+const resetForm = () => {
+    document.querySelector("#fullName").value = "";
+    document.querySelector("#empCode").value = "";
+    document.querySelector("#salary").value = "";
+    document.querySelector("#city").value = "";
     selectedRow = null;
 }
 
-function onEdit(td) {
+// Ações realizadas ao clicar no botão editar. Ele grava as informações da tabela no formulário para ser editado.
+const onEdit = (td) => {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+    document.querySelector("#fullName").value = selectedRow.cells[0].innerHTML;
+    document.querySelector("#empCode").value = selectedRow.cells[1].innerHTML;
+    document.querySelector("#salary").value = selectedRow.cells[2].innerHTML;
+    document.querySelector("#city").value = selectedRow.cells[3].innerHTML;
 }
-function updateRecord(formData) {
+
+// UPDATE
+const updateRecord = (formData) => {
     selectedRow.cells[0].innerHTML = formData.fullName;
     selectedRow.cells[1].innerHTML = formData.empCode;
     selectedRow.cells[2].innerHTML = formData.salary;
     selectedRow.cells[3].innerHTML = formData.city;
 }
 
-function onDelete(td) {
-    if (confirm('Are you sure to delete this record ?')) {
+// DELETE
+const onDelete = (td) => {
+    if (confirm('Tem certeza que deseja excluir este registro ?')) { // OBS.: a função confirme é nativa do javascript para exibir um POPUP de confirmação
         row = td.parentElement.parentElement;
-        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        document.querySelector("#employeeList").deleteRow(row.rowIndex);
         resetForm();
     }
 }
 
-function validate() {
+// Função de validação
+const validate = () => {
     isValid = true;
-    if (document.getElementById("fullName").value == "") {
+    if (document.querySelector("#fullName").value == "") {
         isValid = false;
         document.getElementById("fullNameValidationError").classList.remove("hide");
     } else {
@@ -83,6 +89,7 @@ function validate() {
     return isValid;
 }
 
+// Adicionando ao LocalStorage
 localStorage.setItem('items', JSON.stringify(newRow));
 const data = JSON.parse(localStorage.getItem('items'));
 
@@ -91,5 +98,3 @@ const liMaker = (text) => {
   li.textContent = text;
   ul.appendChild(li);
 }
-
-localStorage.setItem('items', )
